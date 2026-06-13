c3b0dd8c4d4c4669f9a5ede59d79272f6f2e8ecd
PranjalShridhar316 <pranjalshridhar9259@gmail.com>
2026-06-13 01:51:42 +0000

    Add initial SlowPro entry point

---
+++ slowpro.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#echo "======================================"
+#echo "             SlowPro"
+#echo "Linux Forensics . Threat Hunting ."
+#echo "        Incident Response"
+#echo "======================================"
